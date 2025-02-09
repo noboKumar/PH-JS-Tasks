68060a00f1a4dee5cad80f9e13d0914c30209294
noboKumar <nobokumar227@gmail.com>
2025-02-09 17:39:59 +0600

    add conditional t1

---
+++ conditional/Task1.js
@@ -0,0 +1,13 @@
+//Task-1:
+/*
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+//solution:
+let burgerPrice = 600;
+if (burgerPrice > 500) {
+  console.log("You get a free coke");
+} else {
+  console.log("coke is 30tk");
+}
