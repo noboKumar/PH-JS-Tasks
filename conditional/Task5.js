6a581987a1043137174a387844146041dccf7792
noboKumar <nobokumar227@gmail.com>
2025-02-09 18:07:05 +0600

    add condition t5

---
+++ conditional/Task5.js
@@ -0,0 +1,19 @@
+// Task-5
+/***
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+// solution:
+
+let num1 = 50;
+let num2 = 20;
+let result = num1 > num2 ? num1 * 2 : num1 + num2;
+
+console.log(result);
