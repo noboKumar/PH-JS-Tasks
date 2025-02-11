3e1cfd07cf859789931eae3fe4f9333ef6f0eed8
noboKumar <nobokumar227@gmail.com>
2025-02-12 01:14:09 +0600

    add string task4

---
+++ string/task4.js
@@ -0,0 +1,15 @@
+/**
+ * Task-4:
+ * If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+ *
+ * Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+ */
+
+const str = "Xexox";
+if (str.toLowerCase().includes("x")) {
+  let newStr = str.replace("x", "y").replace("X", "Y");
+  console.log(newStr);
+} else if (str.toLowerCase().includes("y")) {
+  let newStr = str.replace("y", "x").replace("Y", "X");
+  console.log(newStr);
+}
