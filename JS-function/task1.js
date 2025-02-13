cec5a560f3aa0dc29b62d9f5358fb21958e9dc64
noboKumar <nobokumar227@gmail.com>
2025-02-13 20:47:35 +0600

    add js-function task1

---
+++ JS-function/task1.js
@@ -0,0 +1,10 @@
+/* 
+Task-1
+Take four parameters. Multiply the four numbers and then return the result
+*/
+
+function muiltiply (num1, num2, num3, num4){
+    const result = num1 * num2 * num3 * num4;
+    return result;
+}
+console.log(muiltiply(1, 2, 3, 4));
