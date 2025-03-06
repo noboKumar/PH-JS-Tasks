b51090bcb170ec2e8b9d0a42521a4885b080d885
noboKumar <nobokumar227@gmail.com>
2025-03-06 17:20:28 +0600

    Add array square with map

--- ES6/map.js
+++ ES6/map.js
@@ -1,10 +1,13 @@
 const users = [
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 30 },
-    { name: "Charlie", age: 35 }
-  ];
-  
-  const names = users.map(user => user.name);
-  console.log(names);
-  // Output: ["Alice", "Bob", "Charlie"]
-  
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 35 },
+];
+
+const names = users.map((user) => user.name);
+// console.log(names);
+
+// array square:
+const arr = [1, 3, 5, 6, 78, 8, 754];
+const newArr = arr.map((num) => num * num);
+console.log(newArr);
