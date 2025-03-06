ade4aa7eafb22dea24dc5494aa950bc18247b1c4
noboKumar <nobokumar227@gmail.com>
2025-03-06 17:13:06 +0600

    Add map method

---
+++ ES6/map.js
@@ -0,0 +1,10 @@
+const users = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+  ];
+  
+  const names = users.map(user => user.name);
+  console.log(names);
+  // Output: ["Alice", "Bob", "Charlie"]
+  
